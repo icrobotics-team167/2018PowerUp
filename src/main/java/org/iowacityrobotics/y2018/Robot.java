80122c21d85fb7b7240d7d970167b3580a6baec8
phantamanta44 <evandalong@gmail.com>
2018-01-25 20:28:02 -0600

    Throw out useless stuff

--- src/main/java/org/iowacityrobotics/y2018/Robot.java
+++ src/main/java/org/iowacityrobotics/y2018/Robot.java
@@ -1,39 +1,52 @@
 package org.iowacityrobotics.y2018;
 
-import com.kauailabs.navx.frc.AHRS;
-import edu.wpi.first.wpilibj.SPI;
-import org.iowacityrobotics.roboed.data.Data;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import org.iowacityrobotics.roboed.data.sink.Sink;
 import org.iowacityrobotics.roboed.data.source.Source;
-import org.iowacityrobotics.roboed.robot.Flow;
 import org.iowacityrobotics.roboed.robot.IRobotProgram;
-import org.iowacityrobotics.roboed.robot.RobotMode;
 import org.iowacityrobotics.roboed.subsystem.SinkSystems;
 import org.iowacityrobotics.roboed.util.math.Vector4;
 import org.iowacityrobotics.roboed.util.robot.MotorTuple4;
 
-import java.util.concurrent.TimeUnit;
-
 public class Robot implements IRobotProgram {
+
+    // Ramp
+    Source<Double> srcRampServo;
+    Sink<Double> snkRampServo;
+    Source<DoubleSolenoid.Value> srcRampPiston;
+    Sink<DoubleSolenoid.Value> snkRampPiston;
+
+    // Lift
+    Source<Double> srcLift;
+    Sink<Double> snkLift;
+
+    // Drive
+    Source<Vector4> srcDrive;
+    Sink<Vector4> snkDrive;
+
+    // Intake
+    Source<Double> srcIntakeFwd;
+    Source<Double> srcIntakeRev;
+    Sink<Double> snkIntakeFwd;
+    Sink<Double> snkIntakeRev;
 
     @Override
     public void init() {
+        // Ramp
+        snkRampServo = SinkSystems.MOTOR.servo(0);
+        snkRampPiston = SinkSystems.OTHER.dblSolenoid(0, 0);
+
+        // Lift
+        snkLift = SinkSystems.MOTOR.talonSrx(0);
+
+        // Drive
         MotorTuple4 motors = MotorTuple4.ofTalons(1, 4, 3, 6);
         motors.getFrontRight().setInverted(true);
         motors.getRearRight().setInverted(true);
-        Sink<Vector4> snkDrive = SinkSystems.DRIVE.mecanum(motors);
-        AHRS gyro = new AHRS(SPI.Port.kMXP);
-        RobotMode.AUTO.setOperation(() -> {
-            Vector4 vec = new Vector4(0, 0.45, 0, 0);
-            Source<Vector4> src = Data.source(() -> vec);
-            snkDrive.bind(src);
-            Flow.waitFor(1, TimeUnit.SECONDS);
+        snkDrive = SinkSystems.DRIVE.mecanum(motors);
 
-            Vector4 vec2 = new Vector4(0, 0, -0.26, 0);
-            Source<Vector4> src2 = Data.source(() -> vec2);
-            double initialAngle = gyro.getAngle();
-            snkDrive.bind(src2);
-            Flow.waitUntil(() -> Math.abs(gyro.getAngle() - initialAngle) >= 90);
-        });
+        // Intake
+        snkIntakeFwd = SinkSystems.MOTOR.talonSrx(0);
+        snkIntakeRev = SinkSystems.MOTOR.talonSrx(0);
     }
 }
