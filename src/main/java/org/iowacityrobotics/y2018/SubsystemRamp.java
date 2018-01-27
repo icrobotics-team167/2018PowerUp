0c975859394ace1c472942befc51c8f1ccd39b45
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2018-01-27 16:28:07 -0600

    Started on ramp subsystem. (Vlad worked too)

--- src/main/java/org/iowacityrobotics/y2018/SubsystemRamp.java
+++ src/main/java/org/iowacityrobotics/y2018/SubsystemRamp.java
@@ -2,8 +2,13 @@ package org.iowacityrobotics.y2018;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import org.iowacityrobotics.roboed.data.source.Source;
+import org.iowacityrobotics.roboed.subsystem.MapperSystems;
+import org.iowacityrobotics.roboed.subsystem.SourceSystems;
 
 public class SubsystemRamp {
+
+    private static final Source<Double> btn = SourceSystems.CONTROL.button(1, 3)
+            .map(MapperSystems.CONTROL.buttonValue(0D, 0.5));
 
     public static Source<Double> getServo() {
         // TODO Implement
