93106c11dfb139e626772de44eea215338ebeb26
Calvin Greene <calvin.greene@gmail.com>
2018-02-06 20:23:17 -0600

    Fixed Ultrasonic subsystem again Vlad + Dominic helped too

--- src/main/java/org/iowacityrobotics/y2018/subsystem/SubsystemUltrasonic.java
+++ src/main/java/org/iowacityrobotics/y2018/subsystem/SubsystemUltrasonic.java
@@ -1,12 +1,14 @@
 package org.iowacityrobotics.y2018.subsystem;
 
+import org.iowacityrobotics.roboed.data.Data;
 import org.iowacityrobotics.roboed.data.source.Source;
 import org.iowacityrobotics.roboed.subsystem.SourceSystems;
 
 public class SubsystemUltrasonic {
 
-    public static Source<Integer> get() {
-        return SourceSystems.SENSOR.analog(0);
+    public static Source<Double> get() {
+        return SourceSystems.SENSOR.analog(0)
+                .map(Data.mapper(a -> (double) a));
     }
 
 }
