62593b95767d51ff0cf031d6e031e2638a2e989f
phantamanta44 <evandalong@gmail.com>
2018-02-14 17:37:59 -0600

    More funky control changes

--- src/main/java/org/iowacityrobotics/y2018/subsystem/SubsystemIntake.java
+++ src/main/java/org/iowacityrobotics/y2018/subsystem/SubsystemIntake.java
@@ -4,18 +4,20 @@ import org.iowacityrobotics.roboed.data.Data;
 import org.iowacityrobotics.roboed.data.source.Source;
 import org.iowacityrobotics.roboed.subsystem.MapperSystems;
 import org.iowacityrobotics.roboed.subsystem.SourceSystems;
+import org.iowacityrobotics.roboed.util.math.Maths;
 
 public class SubsystemIntake {
 
     private static final Source<Double> srcOut = SourceSystems.CONTROL.axis(2, 2)
-            .map(MapperSystems.CONTROL.booleanify(0.5D))
-            .map(MapperSystems.CONTROL.buttonValue(0D, 0.42D));
+            .map(MapperSystems.CONTROL.deadZoneD(0.05D));
 
     private static final Source<Double> srcIn = SourceSystems.CONTROL.axis(2, 3)
             .map(MapperSystems.CONTROL.booleanify(0.5D))
-            .map(MapperSystems.CONTROL.buttonValue(0D, -0.7D));
+            .map(MapperSystems.CONTROL.buttonValue(0D, -1D));
 
-    private static final Source<Double> srcCombined = srcOut.inter(srcIn, Data.inter((a, b) -> a + b));
+    private static final Source<Double> srcCombined = srcOut
+            .inter(srcIn, Data.inter((a, b) -> a + b))
+            .map(Data.mapper(v -> Maths.clamp(v, -1D, 1D)));
 
     public static Source<Double> getForwards() {
         return srcCombined;
--- src/main/java/org/iowacityrobotics/y2018/subsystem/SubsystemLift.java
+++ src/main/java/org/iowacityrobotics/y2018/subsystem/SubsystemLift.java
@@ -1,12 +1,14 @@
 package org.iowacityrobotics.y2018.subsystem;
 
 import org.iowacityrobotics.roboed.data.source.Source;
+import org.iowacityrobotics.roboed.subsystem.MapperSystems;
 import org.iowacityrobotics.roboed.subsystem.SourceSystems;
 
 public class SubsystemLift {
 
     public static Source<Double> get() {
-        return SourceSystems.CONTROL.axis(2, 1);
+        return SourceSystems.CONTROL.axis(2, 1)
+                .map(MapperSystems.CONTROL.deadZoneD(0.08D));
     }
 
 }
