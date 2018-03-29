91feb543deec9eb17986c5a3e6340c0c845ae355
Iowa City Robotics <icroboticsteam167@googlegroups.com>
2018-03-28 19:46:49 -0500

    Defer 12 inches of forwards driving in center routine

--- src/main/java/org/iowacityrobotics/y2018/auto/RoutineCenter.java
+++ src/main/java/org/iowacityrobotics/y2018/auto/RoutineCenter.java
@@ -6,13 +6,13 @@ import org.iowacityrobotics.y2018.Robot;
 public class RoutineCenter implements IAutoRoutine {
     @Override
     public void doTheAutoThing(Robot bot, int mult) {
-        AutoUtil.drive(bot, 24D, 0.9D); // drive forwards a bit
+        AutoUtil.drive(bot, 12D, 0.9D); // drive forwards a bit
         Logs.info("step 1 done");
         AutoUtil.strafeBlind(bot, 1350L + 335L * mult, mult * -1D); // strafe towards side with active switch
         Logs.info("step 2 done");
 
         bot.liftController.set(0.5D); // instruct the lift to hold the cube in the air
-        AutoUtil.driveWithTimeout(bot, 79D, 0.9D, 2000L); // drive forwards to switch
+        AutoUtil.driveWithTimeout(bot, 91D, 0.9D, 2000L); // drive forwards to switch
         Logs.info("step 3 done");
 
         AutoUtil.skillshot(bot, true); // release the cube
